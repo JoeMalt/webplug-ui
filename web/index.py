8b5d8ff87ca270f001f594b3da2d6cd434ef16ec
Joe Malt <joemalt1832@gmail.com>
2016-08-24 13:52:32 +0100

    Changes to login, not currently working

--- web/index.py
+++ web/index.py
@@ -1,16 +1,13 @@
 from flask import *
+from functools import wraps
 app = Flask(__name__)
 
 #Config
 app.config['SECRET_KEY'] = "sdfharwifsfhqht89qthehfq938rutu9e4ufgpWQEUFQEUF498"
 
 @app.route("/")
+@login_required
 def index():
-	if 'username' not in session.keys() or session['username'] == None:
-		#User is not logged in
-		flash("You must log in to access this page")
-		return redirect(url_for('login'))
-		
 	return render_template('index.html')
 
 @app.route("/login", methods = ['POST', 'GET'])
@@ -47,7 +44,24 @@ def toggle():
 	return redirect(url_for('index'))
 	
 
-#This function should probably go elsewhere
+#These functions should probably go elsewhere
+
+def is_logged_():
+	if 'username' not in session.keys() or session['username'] == None:
+			#User is not logged in
+			flash("You must log in to access this page")
+			redirect(url_for('login'))
+			return False
+	return True
+	
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session.keys() or session['username'] == None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+   
 def verify_credentials(username, password):
 	return True
 
