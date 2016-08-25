ba75df4ec71d99c08b7546a6cb82a2cb2389fbe5
ebaines <euanbaines@gmail.com>
2016-08-25 11:16:51 +0100

    Reformatted file.

--- web/__init__.py
+++ web/__init__.py
@@ -3,7 +3,6 @@ from functools import wraps
 from flask import *
 from passlib.apps import custom_app_context as pwd_context
 
-
 from core.orm_template import db_user, db_plugSocket
 from core import get_db_session
 
@@ -22,7 +21,7 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
-    
+
 
 def admin_required(f):
     @wraps(f)
@@ -33,6 +32,7 @@ def admin_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
 
 @app.route("/")
 @login_required
@@ -56,7 +56,7 @@ def login():
         password = request.form['password']
 
         login_status = verify_credentials(username, password)
-        
+
         if login_status == 1:
             session['username'] = username
             session['is_admin'] = False
@@ -71,36 +71,40 @@ def login():
             flash("Incorrect username or password")
             return redirect(url_for('login'))
 
+
 @app.route("/admin")
 @login_required
 @admin_required
 def admin():
     return render_template("admin.html")
-    
+
+
 @app.route("/admin_change_password/<user_id>", methods=['GET'])
 @login_required
 @admin_required
 def admin_change_password(user_id):
     return render_template("admin_change_password.html")
-    
+
+
 @app.route("/delete_user", methods=['POST'])
 @login_required
 @admin_required
 def delete_user():
     user_id = request.form['user_id']
-    #database: delete user with user_id
+    # database: delete user with user_id
     flash("User deleted")
     return redirect(url_for('admin'))
-    
+
+
 @app.route("/toggle_admin", methods=['POST'])
 @login_required
 @admin_required
 def toggle_admin():
     user_id = request.form['user_id']
-    #database: toggle is_admin for user with user_id
+    # database: toggle is_admin for user with user_id
     flash("Admin status toggled")
     return redirect(url_for('admin'))
-    
+
 
 @app.route("/admin_change_password", methods=['POST'])
 @login_required
@@ -112,7 +116,8 @@ def admin_change_password_process():
         return redirect(url_for('admin'))
     else:
         flash("Passwords do not match")
-        return redirect(url_for('admin_change_password', user_id = 1))
+        return redirect(url_for('admin_change_password', user_id=1))
+
 
 @app.route("/logout")
 @login_required
@@ -133,6 +138,7 @@ def toggle():
     flash('Toggled plug ' + plug + ' on host ' + host + '.')
     return redirect(url_for('index'))
 
+
 def verify_credentials(username, password):
     # Returns 0 for bad credentials, 1 for standard user, 2 for admin
     # We need to guarantee closure of the database.
@@ -148,5 +154,3 @@ def verify_credentials(username, password):
         return 1
     else:
         return 0
-        
-
